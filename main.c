6757a8ce7b00814303113075f909776498e69904
Bas van Tiel <bas.van.tiel@axon.nl>
2019-12-30 16:17:05 +0100

    intial setup

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "libuv/include/uv.h"
+
 int main(int argc, char *argv[], char *env[])
 {
-  return 0;
+    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+
+    printf("Now quitting.\n");
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    uv_loop_close(loop);
+    free(loop);
+    return 0;
 }
