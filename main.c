23c57ca1186f4fac493d968295a88f703d7647a6
Bas van Tiel <bas.van.tiel@axon.nl>
2019-12-31 14:00:18 +0100

    timer functions

--- main.c
+++ main.c
@@ -30,6 +30,12 @@ static void term_reset()
     ioctl(0, TCSETS, &oldT);
 }
 
+static void usage(void)
+{
+    fprintf(stderr, "q(uit)\n");
+    fprintf(stderr, "h(elp)\n");
+}
+
 void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
     if (nread < 0){
         if (nread == UV_EOF){
@@ -43,7 +49,7 @@ void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
             uv_stop(loop);
             break;
             case 'h':
-            fprintf(stderr, "usage\n");
+            usage();
             break;
         }
     }
@@ -51,6 +57,19 @@ void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
     // OK to free buffer as write_data copies it.
     if (buf->base)
         free(buf->base);
+}
+
+void signal_handler(uv_signal_t *req, int signum)
+{
+    fprintf(stderr, "Caugth signal %d\n", signum);
+    uv_stop(loop);
+    uv_signal_stop(req);
+}
+static uv_timer_t timer;
+
+static void timer_func(uv_timer_t* handle)
+{
+    fprintf(stderr, "timer called, missing cookie %d\n", (int)uv_handle_get_data(handle));
 }
 
 int main(int argc, char *argv[], char *env[])
@@ -62,12 +81,22 @@ int main(int argc, char *argv[], char *env[])
     uv_pipe_init(loop, &stdin_pipe, 0);
     uv_pipe_open(&stdin_pipe, 0);
 
+    uv_signal_t sig;
+    uv_signal_init(loop, &sig);
+    uv_signal_start(&sig, signal_handler, SIGINT);
+
     uv_read_start((uv_stream_t*)&stdin_pipe, alloc_buffer, read_stdin);
+
+    uv_timer_init(loop, &timer);
+    uv_timer_start(&timer, timer_func, 1, 2000);
+    uv_handle_set_data(&timer, 4);
 
     //MAINLOOP
     uv_run(loop, UV_RUN_DEFAULT);
 
+    uv__pipe_close(&stdin_pipe);
     uv_loop_close(loop);
+    uv_close(&timer, 0);
     term_reset();
     free(loop);
     return 0;
