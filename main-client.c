7724511c399da5622859ce9687f2e0af7af3f89a
Bas van Tiel <bas.van.tiel@axon.nl>
2020-01-01 17:17:09 +0100

    readonly tcp client

--- main-client.c
+++ main-client.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <termios.h>
-
+#include "list.h"
+#include "utils.h"
 #include "libuv/include/uv.h"
 
 static uv_handle_t stdin_pipe;
 static uv_loop_t *loop;
 static struct termios oldT, newT;
-static uv_timer_t timer;
+struct sockaddr_in addr;
 
 static void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
     *buf = uv_buf_init((char*) malloc(suggested_size), suggested_size);
@@ -68,13 +69,57 @@ static void signal_handler(uv_signal_t *req, int signum)
     uv_signal_stop(req);
 }
 
-static void timer_func(uv_timer_t* handle)
+typedef struct {
+    uv_write_t req;
+    uv_buf_t buf;
+} write_req_t;
+
+struct client_
 {
-    fprintf(stderr, "timer called, missing cookie %d\n", (int)uv_handle_get_data(handle));
+    struct list_tag list;
+    uv_tcp_t client;
+    uv_stream_t *server;
+};
+
+static void free_write_req(uv_write_t *req) {
+    write_req_t *wr = (write_req_t*) req;
+    fprintf(stderr, "free write req\n");
+    free(wr);
+}
+
+static void on_close(uv_handle_t* handle) {
+    fprintf(stderr, "on close\n");
+}
+
+static void echo_write(uv_write_t *req, int status) {
+    if (status) {
+        fprintf(stderr, "Write error %s\n", uv_strerror(status));
+    }
+    free_write_req(req);
+}
+
+static void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+    if (nread > 0) {
+        fprintf(stderr, "[%s]\n", buf->base);
+        return;
+    }
+    if (nread < 0) {
+        if (nread != UV_EOF)
+            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+        uv_close((uv_handle_t*) client, on_close);
+    }
+
+    free(buf->base);
+}
+
+static void on_connect(uv_connect_t *req, int status)
+{
+  uv_read_start((uv_stream_t*)req->handle, alloc_buffer, echo_read);
 }
 
 int main(int argc, char *argv[], char *env[])
 {
+    int r;
     loop = malloc(sizeof(uv_loop_t));
     uv_loop_init(loop);
     term_init();
@@ -88,17 +133,30 @@ int main(int argc, char *argv[], char *env[])
 
     uv_read_start((uv_stream_t*)&stdin_pipe, alloc_buffer, read_stdin);
 
-    uv_timer_init(loop, &timer);
-    uv_timer_start(&timer, timer_func, 1, 2000);
-    uv_handle_set_data(&timer, 4);
+    uv_tcp_t client;
+    uv_tcp_init(loop, &client);
+
+    uv_ip4_addr("127.0.0.1", 7000, &addr);
+
+    uv_connect_t connect_req;
+    client.data = &connect_req;
+    r = uv_tcp_connect(&connect_req,
+                     &client,
+                     (const struct sockaddr*) &addr,
+                     on_connect);
+
+    if (r) {
+        fprintf(stderr, "Cannot connect %s\n", uv_strerror(r));
+        return 1;
+    }
 
     //MAINLOOP
-    uv_run(loop, UV_RUN_DEFAULT);
+    r = uv_run(loop, UV_RUN_DEFAULT);
 
+    uv_close(&client, on_close);
     uv__pipe_close(&stdin_pipe);
     uv_loop_close(loop);
-    uv_close(&timer, 0);
     term_reset();
     free(loop);
-    return 0;
+    return r;
 }
